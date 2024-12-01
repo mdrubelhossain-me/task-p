46f72be73c3a071527fc16c7aaedba21f967c907
mdrubelhossain-me <mdrubelhossain.cse@gmail.com>
2024-12-02 04:35:53 +0600

    hero section Mobile responsive css added

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Hero from '../Hero/Hero';
+import PopularCourses from '../PopularCourses/PopularCourses';
 
 const Home = () => {
     return (
         <div>
             <Hero/>
+            <PopularCourses/>
         </div>
     );
 };
---
+++ src/Component/PopularCourses/PopularCourses.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PopularCourses = () => {
+    return (
+        <div className='pt-5 pb-5'>
+            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem quas ea laudantium impedit consectetur, harum vel eligendi esse illum eaque facilis eos fugiat, odit placeat veritatis culpa quibusdam ut ad velit veniam totam sit repellendus. Assumenda deserunt quasi eaque dignissimos porro culpa magnam. Delectus ipsam aliquam laboriosam placeat cumque magni facere atque nemo voluptates incidunt officiis minima, modi facilis suscipit! Et consectetur repellendus cupiditate illum inventore, doloribus deserunt aut, ea doloremque ex necessitatibus laudantium assumenda maxime non ullam, est vel! Vero neque excepturi aliquam quibusdam odit blanditiis obcaecati vitae, magnam delectus deserunt quaerat velit minus debitis animi modi, ut nesciunt ex molestiae deleniti ad. Aliquid, nemo. Provident, vitae soluta deserunt aperiam illo voluptas qui optio nemo sed, autem est voluptates voluptatum deleniti assumenda ullam, aliquid dolores accusamus voluptatibus tempora corporis dolor doloremque atque at. Error ullam explicabo inventore fuga dolores velit atque ea molestiae nobis iure modi laborum quasi consequuntur, nostrum ipsum deleniti fugit nihil dolorem ab quae at obcaecati similique! Perferendis ratione eius, sit, aperiam, officia error corporis repellat possimus maxime modi totam dicta labore voluptates eos nesciunt accusantium eligendi nisi quasi. Pariatur rem eius voluptatum, dolor corporis ullam excepturi natus placeat aut sit necessitatibus blanditiis sequi cupiditate fuga.</h1>
+        </div>
+    );
+};
+
+export default PopularCourses;
