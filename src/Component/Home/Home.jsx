46d41d7374285f4d717176f84ee0d1cac94dc54d
mdrubelhossain-me <mdrubelhossain.cse@gmail.com>
2024-12-02 22:56:07 +0600

    font family added in index css

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from '../Hero/Hero';
 import PopularCourses from '../PopularCourses/PopularCourses';
+import Services from '../Services/Services';
 
 const Home = () => {
     return (
         <div>
             <Hero/>
             <PopularCourses/>
+            <Services/>
         </div>
     );
 };
---
+++ src/Component/Services/Services.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Services.css'
+
+const Services = () => {
+    return (
+        <div className='services-section'>
+            <div className='container'>
+                <div className="row g-3">
+                    <div className="col-lg-6 col-md-6 col-sm-6">
+                    <h2 className="title">We provide...</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Services;
