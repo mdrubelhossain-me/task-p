047606421a4921e97cd0902abbea3249934baaee
mdrubelhossain-me <mdrubelhossain.cse@gmail.com>
2024-12-03 01:43:54 +0600

    footer working

---
+++ src/Component/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <div className='footer-section pt-5 pb-5'>
+            
+        </div>
+    );
+};
+
+export default Footer;
